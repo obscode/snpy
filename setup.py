5fb0127bdce9269907d7c490de8f7b84cd575a5c
Chris Burns <cburns@obs.carnegiescience.edu>
2012-05-07 00:37:01 +0000

    updated to include update-snpy script in the installation

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ def configuration(parent_package='', top_path=None):
    config.add_subpackage('utils')
    config.add_subpackage('sqlmod')
    config.add_scripts(['bin/snpy'])
+   config.add_scripts(['bin/update-snpy'])
    config.add_data_dir('typeIa')
    config.add_data_files('ipythonrc-SN')
    return config
