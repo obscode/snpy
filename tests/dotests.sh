806d56ee33d637727239a154107b72c45ca04d4a
Chris Burns <cburns@obs.carnegiescience.edu>
2016-04-07 14:47:42 -0700

    added test for bolometric calculator

--- tests/dotests.sh
+++ tests/dotests.sh
@@ -2,6 +2,7 @@
 
 rm *.eps
 rm *.snpy
+rm *.dat
 
 echo "Testing load"
 ~/snpy2/bin/python test_load.py
@@ -13,4 +14,5 @@ echo "Testing fitting"
 ~/snpy2/bin/python test_fits.py
 echo "Testing k-corrections"
 ~/snpy2/bin/python test_kcorr.py
-
+echo "Testing bolometric calculator"
+~/snpy2/bin/pyhton test_bolometric.py
