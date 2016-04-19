18a8dabc28d7069391aeaf8694ebad7324ffa3f9
Chris Burns <cburns@obs.carnegiescience.edu>
2016-04-19 11:49:28 -0700

    Updated the tests

--- tests/dotests.sh
+++ tests/dotests.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+#export PY=/Users/cburns/snpy2/bin/python
+export PY=python
+
 rm *.eps
 rm *.snpy
 rm *.dat
 
 echo "Testing load"
-~/snpy2/bin/python test_load.py
+$PY test_load.py
 echo "Testing templates"
-~/snpy2/bin/python test_templates.py
+$PY test_templates.py
 echo "Testing plots"
-~/snpy2/bin/python test_plots.py
+$PY test_plots.py
 echo "Testing fitting"
-~/snpy2/bin/python test_fits.py
+$PY test_fits.py
 echo "Testing k-corrections"
-~/snpy2/bin/python test_kcorr.py
+$PY test_kcorr.py
 echo "Testing bolometric calculator"
-~/snpy2/bin/pyhton test_bolometric.py
+$PY test_bolometric.py
