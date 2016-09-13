723503c62e0ada2ac9550068aa93b4ad800bedbd
Chris Burns <cburns@obs.carnegiescience.edu>
2016-09-13 15:19:50 -0700

    Major re-structuring of the folders to make it a bit more standard (and clean up the root github folder)

--- setup.py
+++ setup.py
@@ -1,60 +1,82 @@
 #!/usr/bin/env python
 import os,sys, string
+from pkg_resources import parse_version
 try:
    import matplotlib
    have_mpl = 1
 except:
    have_mpl = 0
 
+numpy_min_version = '1.7'
+
+def get_numpy_status():
+   """
+      Returns a dictionary containing a boolean specifying whether NumPy
+      is up-to-date, along with the version string (empty string if
+      not installed).
+   """
+   numpy_status = {}
+   try:
+      import numpy
+      numpy_version = numpy.__version__
+      numpy_status['up_to_date'] = parse_version(
+         numpy_version) >= parse_version(numpy_min_version)
+      numpy_status['version'] = numpy_version
+   except ImportError:
+      numpy_status['up_to_date'] = False
+      numpy_status['version'] = ""
+   return numpy_status
+
+
 def configuration(parent_package='', top_path=None):
-   config = Configuration('snpy', parent_package, top_path)
-   config.add_subpackage('dm15temp')
-   config.add_subpackage('CSPtemp')
-   config.add_subpackage('filters')
-   config.add_subpackage('spline2')
-   #config.add_subpackage('dust_getval')
-   config.add_subpackage('tspack')
-   config.add_subpackage('utils')
-   config.add_subpackage('sqlmod')
-   config.add_subpackage('SwiftTemp')
+   config = Configuration(None, parent_package, top_path)
+   config.add_subpackage('snpy')
    config.add_scripts(['bin/snpy'])
    config.add_scripts(['bin/update-snpy'])
-   config.add_data_dir('typeIa')
    config.add_data_files('ipythonrc-SN')
-   config.add_data_files('st_calibration2.dat')
-   config.add_data_files('dm15_calibration2.dat')
-   config.add_data_files('BmX_Uniform_u_all.pickle')
-   config.add_data_files('color_priors.pickle')
    return config
 
 
 if __name__ == '__main__':
-   # First, let's see if the user has any extra options
-   extra_options = string.join(sys.argv[2:])
-   try:
-      from numpy.distutils.core import setup
-   except ImportError:
-      print "You don't have numpy installed.  Would you like me to try to"
-      print "install it?  (y/n)"
-      answer = ""
-      while answer != 'n' and answer != 'y':
-         answer = raw_input()
-      if answer == 'n':
-         print "Okay, but you'll need to install Numpy before installing SNPY"
-         sys.exit(1)
-      
-      # So, at this point, we're going to try to install numpy:
-      flag,mesg = get_package('numpy',
-            'http://sourceforge.net/projects/numpy/files/NumPy/1.2.1/numpy-1.2.1.tar.gz/download',
-            options=extra_options)
-      if flag != 1:
-         print mesg
-         sys.exit(1)
+   status = get_numpy_status()
+   if not status['up_to_date']:
+      raise ImportError("You need at least version {} to run SNooPy"
+                        .format(numpy_min_version))
+
    from numpy.distutils.core import setup
    from numpy.distutils.misc_util import Configuration
    from numpy.distutils.system_info import get_info
 
-   setup(version='2.0b',
+   with open(os.path.join('snpy','version.py'), 'r') as fi:
+      line = fi.readline()
+      v = line.split('=')[1].strip()
+      version = v[1:-1]
+
+   setup(version=version,
+         description="Supernova light-curve analysis tool",
          author='Chris Burns (Carnegie Observatories)',
-         author_email='cburns@obs.carnegiescience.edu',
+         author_email='cburns@.carnegiescience.edu',
+         url='http://csp.obs.carnegiescience.edu/data/snpy',
+         license='MIT',
+         classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Framework :: IPython',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering :: Astronomy'],
+         requires=[
+            'NumPy (>=1.7)',
+            'SciPy',
+            'pymysql',
+            'pyfits',
+            'matplotlib',
+            'ipython',
+            'gnureadline',
+            'emcee',
+            'pymc',
+            'astropy'],
          **configuration(top_path='').todict())
