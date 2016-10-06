e9f97f0af5617a6fafab2cc99e4213b79ea8eb48
Chris Burns <cburns@obs.carnegiescience.edu>
2016-10-06 14:15:20 -0700

    udpated the spline2 test scripts

--- snpy/spline2/tests/test_spline2.py
+++ snpy/spline2/tests/test_spline2.py
@@ -1,39 +1,32 @@
-#!/usr/bin/env python
-
-from Numeric import *
-from pygplot import *
-import spline2
+import pytest
+import snpy.spline2 as spline2
 import sys
+from numpy import array,arange,sin,pi,absolute,alltrue
 
-data = columns('example1')
-p = Plot()
-p.point(data[0],data[1])
+#data = columns('example1')
+#p = Plot()
+#p.point(data[0],data[1])
 
-tck = spline2.spline2(data[0],data[1], w=1.0/data[2], verbose=1)
-xs = arange(2001)/2000.0*(max(data[0]) - min(data[0])) + min(data[0])
-ys = spline2.evalsp(xs, tck)
-p.line(xs,ys, color='red', label='spline')
-p.plot()
+@pytest.fixture
+def spdata():
+   x = arange(0,2*pi,0.1)
+   y = sin(x)
+   dy = y + 0.01
+   tck = spline2.spline2(x, y, w=1.0/dy)
+   return (x,y,tck)
 
-# Find the extrema:
-xe,ye,si = spline2.eval_extrema(tck)
-dy = (p.ymax - p.ymin)/50.0
-gids = less(si, 0)
-p.point(compress(gids, xe), compress(gids, ye)+dy, color='green', size=2,
-      symbol=854, label='maxima')
-gids = greater(si, 0)
-p.point(compress(gids, xe), compress(gids, ye)-dy, color='green', size=2,
-      symbol=852, label='minima')
 
-# Find the roots:
-value = 40000.0
-roots = spline2.eval_x(value, tck)
-p.line([200,2000],[value,value], color='yellow')
-p.point(roots, roots*0+value, color='yellow', symbol=4, size=2, label='roots')
-      
-p.legend()
-p.replot()
-p.close()
+def test_spline2(spdata):
+   x,y,tck = spdata
+   yeval = spline2.evalsp(x, tck)
+   assert alltrue(absolute(yeval-y) < 0.01)
 
-data2 = columns('example2')
-tck = spline2.spline2(data[0], data[1], w=1.0/data[2], verbose=1)
+def test_extrema(spdata):
+   xe,ye,si = spline2.eval_extrema(tck)
+   assert len(xe) == 2
+
+def test_root(spdata):
+   roots = spline2.eval_x(0, tck)
+   print roots
+   assert len(xe) == 3
+
