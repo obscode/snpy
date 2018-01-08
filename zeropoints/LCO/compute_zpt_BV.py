7c06a2282acf1509244051a99aa7a925b46f7ebf
Chris Burns <cburns@obs.carnegiescience.edu>
2018-01-08 10:59:33 -0800

    Add multiple primary standards to see how this affects the zeropoints

--- zeropoints/LCO/compute_zpt_BV.py
+++ zeropoints/LCO/compute_zpt_BV.py
@@ -5,32 +5,59 @@ from numpy import *
 import sys,os,string
 from snpy.filters import standards,fset
 
-fs = ['B','V','V0','V1']
-Bvega = 0.03
-Vvega = 0.03
-vegaB = standards['Vega']['VegaB']   # Bohlin version 5 (2004)
+fs = ['B','V0','V1','V']
 
-# Need to convert these "standard" vega magnitudes into Natrual magnitudes using Contreras
-#   color transformations:
-# CAUTION:  we need a synthetic mag. for Vega in i', so this filters's zeropoint needs to 
-#   be set FIRST!!!!
+CTs = {'u':(0.046, 0.017),
+       'B':(0.061, 0.022),
+       'g':(-0.014,0.011),
+       'V':(-0.058,0.011),
+       'r':(-0.016,0.015),
+       'i':(-0.002,0.015),
+       'J':(0.016, 0.00),
+       'H':(-0.029, 0.00),
+       'Ydw':(-0.042, 0.00)}
 
-ip_vega = fset['i_40'].synth_mag(vegaB.wave, vegaB.resp)
+def stand2lco(B, V0, V1, V, i):
+   '''Convert photometry from standard to CSP'''
+   Bnat = B - CTs['B'][0]*(B - V)
+   dBnat = abs(CTs['B'][1]*(B - V))
+   V0nat = V - CTs['V'][0]*(V0 - i)
+   dV0nat = abs(CTs['V'][1]*(V0 - i))
+   V1nat = V - CTs['V'][0]*(V1 - i)
+   dV1nat = abs(CTs['V'][1]*(V1 - i))
+   Vnat = V - CTs['V'][0]*(V - i)
+   dVnat = abs(CTs['V'][1]*(V - i))
+   return ((Bnat,V0nat,V1nat,Vnat),(dBnat,dV0nat,dV1nat,dVnat))
 
-# Use the color terms to transform to natural system from Contreras et al. (2010)
-B_vega = Bvega - 0.061*(Bvega - Vvega)
-V_vega = Vvega + 0.058*(Vvega - ip_vega)
-# I derived this color term for the 'temp' V-band by fitting synthetic photometry
-V1_vega = Vvega + 0.044*(Vvega - ip_vega)
+stds = ['VegaB','p177d_stisnic_006','p330e_stisnic_007']
+ip_vega = fset['i_40'].synth_mag(standards['VegaB'])
+std_mags = [[0.030, 0.030, 0.030, 0.030, ip_vega],
+            [14.138, 13.492, 13.492, 13.492, 13.170],
+            [13.658, 13.028, 13.028, 13.028, 12.701]]
 
-vega_mags = [B_vega, V_vega, V_vega, V1_vega]
+# Need to convert these "standard" vega magnitudes into Natrual magnitudes 
+#  using Contreras color transformations:
+# CAUTION:  we need a synthetic mag. for Vega in i', so this filters's 
+#  zeropoint needs to  be set FIRST!!!!
 
-print "Using the following Natural (shifted) magnitudes for Vega:"
-print "B = ",B_vega
-print "V(LC-3014) = ",V_vega
-print "V(LC-3009) = ",V1_vega
-print "V(LC-9844) = ",V_vega
-print "i' = ",ip_vega
+nat_mags = [stand2lco(*m)[0] for m in std_mags]
+d_nat_mags = [stand2lco(*m)[1] for m in std_mags]
+zpts = {}
+for f in fs:
+   zpts[f] = []
+for i,st in enumerate(stds):
+   print "Assuming standard and natural magnitudes for %s:" % (st)
+   for j,f in enumerate(fs):
+      print "   %s  %6.3f    %6.3f +/- %5.3f" \
+            % (f, std_mags[i][j], nat_mags[i][j], d_nat_mags[i][j])
+   print "Zero-points:"
+   for j,f in enumerate(fs):
+      zpt = fset[f].compute_zpt(standards[st], nat_mags[i][j],
+            zeropad=True)
+      zpts[f].append(zpt)
+      print "   %s  %7.4f +/- %6.4f" % (f, zpt, d_nat_mags[i][j])
 
-for i in range(len(fs)):
-   print fs[i],fset[fs[i]].compute_zpt(vegaB, vega_mags[i]), vega_mags[i]
+for f in fs:
+   print "std for %s:  %f" % (f, std(zpts[f]))
+
+
--- zeropoints/LCO/compute_zpt_ugri.py
+++ zeropoints/LCO/compute_zpt_ugri.py
@@ -7,14 +7,29 @@ from snpy.filters import standards
 from snpy.filters import standard_mags
 import scipy
 
+CTs = {'u':(0.046, 0.017),
+       'B':(0.061, 0.022),
+       'g':(-0.014,0.011),
+       'V':(-0.058,0.011),
+       'r':(-0.016,0.015),
+       'i':(-0.002,0.015),
+       'J':(0.016, 0.00),
+       'H':(-0.029, 0.00),
+       'Ydw':(-0.042, 0.00)}
+
+
 # this function converts the USNO40 magnitudes to natural system magnitudes
 def USNO2LCO(up, gp, rp, ip):
    '''Convert Photometric Telescope to CSP'''
-   u = up - 0.046*(up - gp)
-   g = gp + 0.014*(gp - rp)
-   r = rp + 0.016*(rp - ip)
-   i = ip + 0.002*(rp - ip)
-   return(u,g,r,i)
+   u = up - CTs['u'][0]*(up - gp)
+   du = abs(CTs['u'][1]*(up - gp))
+   g = gp - CTs['g'][0]*(gp - rp)
+   dg = abs(CTs['g'][1]*(gp - rp))
+   r = rp - CTs['r'][0]*(rp - ip)
+   dr = abs(CTs['r'][1]*(rp - ip))
+   i = ip - CTs['i'][0]*(rp - ip)
+   di = abs(CTs['i'][1]*(rp - ip))
+   return((u,g,r,i),(du,dg,dr,di))
 
 # Filters to process
 fs = ['u','g','r','i']
@@ -22,47 +37,26 @@ fs = ['u','g','r','i']
 # Standards to use
 stds = ['bd17B','p177d_stisnic_006','p330e_stisnic_007']
 std_mags = [[standard_mags['Smith']['bd17'][f+'_40'] for f in fs],
-            [15.118,13.750,13.302,13.161],
-            [14.548,13.285,12.843,12.704]]
+            [15.128,13.771,13.316,13.170],
+            [14.539,13.287,12.848,12.701]]
 
 # Convert Standard mags to natural mags
-nat_mags = [USNO2LCO(*m) for m in std_mags]
-for i,std in enumerate(stds):
-   print "Assuming standard and natural magnitdes for %s:" % (std)
+nat_mags = [USNO2LCO(*m)[0] for m in std_mags]
+d_nat_mags = [USNO2LCO(*m)[1] for m in std_mags]
+zpts = {}
+for f in fs:
+   zpts[f] = []
+for i,st in enumerate(stds):
+   print "Assuming standard and natural magnitudes for %s:" % (st)
    for j,f in enumerate(fs):
-      print "   %s  %6.3f    %6.3f" % (f, std_mags[i][j], nat_mags[i][j])
+      print "   %s  %6.3f    %6.3f +/- %5.3f" \
+            % (f, std_mags[i][j], nat_mags[i][j], d_nat_mags[i][j])
    print "Zero-points:"
    for j,f in enumerate(fs):
-      zpt = fset[f].compute_zpt(standards[std], nat_mags[i][j],
+      zpt = fset[f].compute_zpt(standards[st], nat_mags[i][j],
             zeropad=True)
-      print "   %s  %7.4f" % (f, zpt)
+      zpts[f].append(zpt)
+      print "   %s  %7.4f +/- %6.4f" % (f, zpt, d_nat_mags[i][j])
 
-
-# Do this three times, just to see who different SED's change the answer
-#print "zero-points with the original BD+174708:"
-#for f in fs:
-#   zpt = fset[f].compute_zpt(sloan_standards['bd17'], sm[f], zeropad=1)
-#   print f, zpt
-#
-#print "zero-points with Mark's modified BD+174708:"
-#for f in fs:
-#   zpt = fset[f].compute_zpt(sloan_standards['bd17adj'], sm[f], 
-#         zeropad=1)
-#   print f, zpt
-#print "zero-points with Bohlin & Gilliland (2004) SED:"
-#for f in fs:
-#   zpt = fset[f].compute_zpt(sloan_standards['bd17B'], sm[f], 
-#         zeropad=1)
-#   print f, zpt
-#
-#print "zero-points with Bohlin & Gilliland (2004) SED:"
-#for f in fs:
-#   zpt = fset[f].compute_zpt(sloan_standards['bd17B'], sm[f], 
-#         zeropad=1)
-#   print f, zpt
-#
-#print "zero-points with Bohlin & Gilliland (2004) SED:"
-#for f in fs:
-#   zpt = fset[f].compute_zpt(sloan_standards['bd17B'], sm[f], 
-#         zeropad=1)
-#   print f, zpt
+for f in fs:
+   print "std for %s:  %f" % (f, std(zpts[f]))
