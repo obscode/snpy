684d9ffc8277adbce9c6e4344fc2bbf9eb46fce4
Chris Burns <cburns@obs.carnegiescience.edu>
2019-01-11 11:38:00 -0800

    get rid of redundant use()

--- tests/test_lira.py
+++ tests/test_lira.py
@@ -1,7 +1,7 @@
 import pytest
 import matplotlib
 import numpy as np
-matplotlib.use("Agg")
+#matplotlib.use("Agg")
 try:
    import astropy
    haveastropy = pytest.mark.skipif(False, reason="astropy is installed")
@@ -17,12 +17,12 @@ def snobj():
    return s
 
 def test_lira_noparams(snobj):
-   EBV,eEBV,slope,eslope = snobj.lira('B','V')
+   EBV,eEBV,slope,eslope = snobj.lira('B','V', deredden=False)
    assert (round(EBV,3) == round(0.0471892119,3) and
            round(eEBV,3) == round(0.004766264,3))
 
 def test_lira_interp_model(snobj, interpolate=True):
-   EBV,eEBV,slope,eslope = snobj.lira('B','V')
+   EBV,eEBV,slope,eslope = snobj.lira('B','V', deredden=False)
    assert (round(EBV,3) == round(0.047188195845567507,3) and
            round(eEBV,3) == round(0.00476626486707499,3))
 
